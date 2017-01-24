cd809f0caaf57c5bd6c61fb0bf60c79e87c0d282
mfitzharris <molly.fitzharris@gmail.com>
2017-01-24 13:54:28 -0600

    View for login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+  def new
+    @user
+  end
+
+  def post
+  end
+
+  def delete
+  end
+
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   get '/users/new' => "users#new"
   post '/users' => "users#create"
   get "/users/:id" => "users#show"
+
+  get '/sessions/new' => "sessions#new"
+  post '/sessions' => "sessions#create"
+  delete '/sessions' => "sessions#destroy"
 end
