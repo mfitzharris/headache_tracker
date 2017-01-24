81e429b7832cbd8045ea76c059cf41e87b38db3d
mfitzharris <molly.fitzharris@gmail.com>
2017-01-24 14:04:54 -0600

    Controller logic for new sessions creation, add error handling to view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,20 @@ class SessionsController < ApplicationController
     @user
   end
 
-  def post
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      sesssion[:user_id] = @user.id
+      redirect_to '/'
+    else
+      @errors = ["Username and/or password do not match our records"]
+      render 'new'
+    end
   end
 
   def delete
+    session[:user_id] = nil
+    redirect_to '/games'
   end
 
   
